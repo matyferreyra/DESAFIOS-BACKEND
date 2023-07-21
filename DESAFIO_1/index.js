c2067fcc9b1fbbf20845b5167d5be8a19bb2cd20
Matias Ferreyra <103839740+matyferreyra@users.noreply.github.com>
2023-07-20 22:18:20 -0300

    .
    .

--- DESAFIO_1/index.js
+++ DESAFIO_1/index.js
@@ -3,19 +3,20 @@ class ProductManager {
         this.products = [];
     }
 
-    addProduct (title, description, price, thumbnail, code, stock) {
+    addProduct (title, description, price, thumbnail, code, stock) { //ESTE METODO ES PARA AGREGAR UN NUEVO PRODUCTO, HACIENDO LAS VALIDACIONES CON LOS OTROS METODOS DECLARADOS.
+        
+        if (this.products.some((product) => product.code === code)) {
+            console.warn ("El código del producto ya existe. Vuelve a intentarlo con otro código");
+            return;
+        }
+
         if (!title || !description || !price || !thumbnail ||!code || !stock){
             console.warn("Todos los campos son obligatorios. Completarlos para poder avanzar");
             return;
-        }
-
-        if (this.products.some((product) => product.code === code)) {
-            console.warn ("El código de producto ya existe");
-            return;
-        }
-
-        const product = {id:this.getId(), title:title, description:description, price:price, thumbnail:thumbnail, code:code, stock:stock}
+        } else { const product = {id:this.getId(), title:title, description:description, price:price, thumbnail:thumbnail, code:code, stock:stock}
         this.products.push(product);
+        console.log("¡Congratulations!. New product added");
+        }       
     }
     
     getId () { //ESTE MÉTODO ES PARA AUTOINCREMENTAR EL ID
@@ -25,13 +26,9 @@ class ProductManager {
             max = product.id > max && product.id;
         })
 
-        return (max ++);
+        return (max ++); //RETORNA EL "ID" MÁXIMO QUE EXISTA EN ESE MOMENTO Y LE SUMA 1 MAS (PARA NO REPETIR "ID" Y TENER UN NUEVO MÁXIMO).
     }
-
-    getProducts () { //ESTE MÉTODO ES PARA DEVOLVER TODO EL ARRAY DE PRODUCTOS QUE EXISTA
-        return this.products;
-    }
-
+    
     getProductById () { //ESTE MÉTODO ES PARA VERIFICAR SI HAY O NO DUPLICIDAD DE ID
         const product = this.products.find(product => product.id === id);
         if (product) {
@@ -41,6 +38,13 @@ class ProductManager {
         }        
     }
 
+    validateCode (code) { //ESTE METODO VALIDA DE QUE NO SE REPITA EL CAMPO "CODE"
+        return this.product.some (product => product.code === code);
+    }
+
+    getProducts () { //ESTE MÉTODO ES PARA DEVOLVER TODO EL ARRAY DE PRODUCTOS QUE EXISTA
+        return this.products;
+    }
 }
 
 //A PARTIR DE ACÁ HACEMOS UNA PRUEBA DEL ALGORITMO
@@ -49,4 +53,7 @@ const productManager = new ProductManager();
 productManager.addProduct("Fernet", "Bebida espirituosa", 1800, "thumbnail1.jpg", "3", 9);
 productManager.addProduct("Ron", "bebida de los piratas", 2150, "thumbnail2.jpg", "2", 7);
 productManager.addProduct("Vodka", "bebida de los soldados rusos", 1500, "thumbnail3.jpg", "1", 11);
-console.log(ProductManager.products());
+console.log(ProductManager.getProducts());
+
+
+
